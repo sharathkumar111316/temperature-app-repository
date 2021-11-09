0586464b34dd86bebdf82052be7dd188eac6f436
sharath <sharathkumar111316@gmail.com>
2021-11-09 17:06:33 +0530

    created mew temperature app

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import './styles.css';
+const App = () => {
+  const [temperatureValue, setTemperatureValue] = useState(10);
+  const [temperatureColor, setTemperatureColor] = useState("cold");
+
+  const increaseTemperature = () => {
+    if (temperatureValue === 30) return;
+
+    const newTemperature = temperatureValue + 1;
+    setTemperatureValue(newTemperature);
+    if (newTemperature >= 15) {
+          setTemperatureColor("hot");
+    }
+
+  };
+
+  const decreaseTemperature = () => {
+    if (temperatureValue === 0) return;
+
+    const newTemperature = temperatureValue - 1;
+    setTemperatureValue(newTemperature);
+    if (newTemperature < 15) {
+          setTemperatureColor("cold");
+    }
+
+  };
+
+  return (
+    <div className="app-container">
+        <div className="temperature-display-container">
+            <div className={`temperature-display ${temperatureColor}`}>{temperatureValue}°C
+            </div>
+        </div>
+        <div className="button-container">
+            <button onClick={() => increaseTemperature()}>+</button>
+            <button onClick={() => decreaseTemperature()}>-</button>            
+        </div>
+    </div>
+  );
+}
+
+export default App;
